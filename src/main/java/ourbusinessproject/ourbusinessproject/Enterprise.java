5311ad5b604585b794c8bfe8938fe8d788607893
Antoine Bessaou <antoine.bessaou@iut-rodez.fr>
2020-02-03 15:00:45 +0100

    fix 1.3.1 persistance Project et Enterprise

--- src/main/java/ourbusinessproject/ourbusinessproject/Enterprise.java
+++ src/main/java/ourbusinessproject/ourbusinessproject/Enterprise.java
@@ -2,11 +2,19 @@ package ourbusinessproject.ourbusinessproject;
 
 import org.hibernate.validator.constraints.Length;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
+@Entity
 public class Enterprise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     @NotEmpty
     private String name, contactName;
--- src/main/java/ourbusinessproject/ourbusinessproject/Project.java
+++ src/main/java/ourbusinessproject/ourbusinessproject/Project.java
@@ -1,8 +1,17 @@
 package ourbusinessproject.ourbusinessproject;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 
+@Entity
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     @NotEmpty
     private String title;
